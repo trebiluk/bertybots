5f82c4ed696d42c5f3c1425d722e346816856a2c
Trebzy <rkulibert@gmail.com>
2026-09-17 17:22:36 -0700

    BB 0.3.0 version, wrangler, Pages workflow

--- js/version.js
+++ js/version.js
@@ -1,6 +1,6 @@
 export const APP_NAME = "Berty's Botz";
 export const APP_PREFIX = "BB";
-export const APP_VERSION = "0.2.1";
+export const APP_VERSION = "0.3.0";
 export const APP_CHANNEL = "live";
-export const APP_CHIP = "BB 0.2.1";
+export const APP_CHIP = "BB 0.3.0";
 export const APP_BUILT = "2026-09-17";
